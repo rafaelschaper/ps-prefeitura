81b809041e9c6dc7b4ce27dd6bc8493e573a187d
Rafael <rafaelschaper@gmail.com>
2026-07-01 16:27:59 -0300

    🐛 fix: delete images from public/storage

--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
+use Throwable;
 
 class Post extends Model
 {
@@ -17,4 +19,14 @@ class Post extends Model
         'image',
         'text',
     ];
+
+    protected static function booted()
+    {
+        self::deleted(function (Post $post) {
+            try {
+                $image_name = explode('posts/', $post['image']);
+                Storage::disk('public')->delete('posts/'.$image_name[1]);
+            }catch (Throwable){} 
+        });
+    }
 }
